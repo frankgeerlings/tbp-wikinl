7ac5ed27a41c903817a9878654560e85fcb2b57d
Frank Geerlings <frank@frankgeerlings.com>
2019-03-20 00:41:40 +0100

    Fix tests

--- nomparse.py
+++
@@ -1,6 +0,0 @@
-import mwparserfromhell
-
-def find_articles(section):
-  wikicode = mwparserfromhell.parse()
-def find_nominator(section):
-def find_dates(section):
--- tests.py
+++ tests.py
@@ -2,10 +2,8 @@ import unittest
 import doctest
 import messagebuilder
 import nomination
-import nomparse
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(messagebuilder))
     tests.addTests(doctest.DocTestSuite(nomination))
-    tests.addTests(doctest.DocTestSuite(nomparse))
     return tests
