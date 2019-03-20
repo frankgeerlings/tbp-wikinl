811d82f147f0f7e4e36ea575b9ea9af0c1c4dfa5
Frank Geerlings <frank@frankgeerlings.com>
2019-03-20 12:21:38 +0100

    Add unit test for signature detection; unnecessary optimization

--- nomination.py
+++ nomination.py
@@ -1,7 +1,7 @@
 from mwparserfromhell import parse
 import itertools, re
 
-class Nomination:
+class Nomination(object):
   def find_pages(self, section):
     title = section.get(0).title
     art_in_title = [unicode(l.title) for l in parse(title).filter_wikilinks()]
@@ -10,23 +10,51 @@ class Nomination:
 
     return set(art_in_title + [unicode(a.value) for a in art_in_linkstemplate])
 
-  def find_r(self, regex, text):
+  @staticmethod
+  def find_r(regex, text):
     r = regex.search(text)
     if (not r is None):
       return (r.start(), r.group('user'))
     else:
       return None
 
-  def find_first_signature(self, section):
+  @staticmethod
+  def find_first_signature(section):
+    """
+    >>> Nomination.find_first_signature('[[User:User name]]')
+    u'User name'
+
+    >>> Nomination.find_first_signature('[[Gebruiker:User name]]')
+    u'User name'
+
+    >>> Nomination.find_first_signature('{{User:User name/Handtekening}}')
+    u'User name'
+
+    >>> Nomination.find_first_signature('{{User:First/Handtekening}} [[User:Second]]')
+    u'First'
+
+    When there are no results, None is returned
+    >>> any([ None, None, None])
+    False
+
+    >>> Nomination.find_first_signature('Blah blah blah') is None
+    True
+    """
     userR = re.compile(r'\[\[(?:[Uu]ser|[Gg]ebruiker):(?P<user>.*?)(?:\|.*?\]\]|\]\])')
     strictTemplateR = re.compile(r'\{\{(?:[Uu]ser|[Gg]ebruiker):(?P<user>.*?)\/[Hh]andtekening\}\}')
     templateR = re.compile(r'\{\{(?:[Uu]ser|[Gg]ebruiker):(?P<user>.*?)\/.*?\}\}')
     
     first = [
-	self.find_r(templateR, unicode(section)) ,
-	self.find_r(userR, unicode(section)) ,
-	self.find_r(strictTemplateR, unicode(section)) ]
- # return section.filter(matches = r'')
+	Nomination.find_r(templateR, unicode(section)) ,
+	Nomination.find_r(userR, unicode(section)) ,
+	Nomination.find_r(strictTemplateR, unicode(section)) ]
+
+    """
+    This handles the case where there are no signatures
+    """
+    if not any(first):
+      return None
+
     return min([x for x in first if x is not None])[1]
 
   def __init__(self, section, pagename):
